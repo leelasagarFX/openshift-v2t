9f7f02a4939af4cac90154b48fe75068ac741ab1
leelasagarfx <leelasagar19803@gmail.com>
2025-04-20 00:43:48 +0530

    Initial commit with Node.js and Express setup

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
